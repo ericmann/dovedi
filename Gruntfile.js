6b7669d490587af7440b5ce13b9a737362c81ca8
Eric Mann <eric@eamann.com>
2015-10-19 23:00:57 -0700

    Remove a bunch of unneeded cruft

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,89 +3,6 @@ module.exports = function( grunt ) {
 	// Project configuration
 	grunt.initConfig( {
 		pkg:    grunt.file.readJSON( 'package.json' ),
-		concat: {
-			options: {
-				stripBanners: true,
-				banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-					' * <%= pkg.homepage %>\n' +
-					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-					' * Licensed GPLv2+' +
-					' */\n'
-			},
-			main: {
-				src: [
-					'assets/js/src/dovedi.js'
-				],
-				dest: 'assets/js/dovedi.js'
-			}
-		},
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'assets/js/src/**/*.js',
-				'assets/js/test/**/*.js'
-			]
-		},
-		uglify: {
-			all: {
-				files: {
-					'assets/js/dovedi.min.js': ['assets/js/dovedi.js']
-				},
-				options: {
-					banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-						' * <%= pkg.homepage %>\n' +
-						' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-						' * Licensed GPLv2+' +
-						' */\n',
-					mangle: {
-						except: ['jQuery']
-					}
-				}
-			}
-		},
-		
-		
-		cssmin: {
-			options: {
-				banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-					' * <%=pkg.homepage %>\n' +
-					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-					' * Licensed GPLv2+' +
-					' */\n',
-				processImport: false
-			},
-			minify: {
-				expand: true,
-
-				cwd: 'assets/css/',
-				src: ['dovedi.css'],
-
-				dest: 'assets/css/',
-				ext: '.min.css'
-			}
-		},
-		watch:  {
-			livereload: {
-				files: ['assets/css/*.css'],
-				options: {
-					livereload: true
-				}
-			},
-			styles: { 
-				files: ['assets/css/*.css', '!assets/css/*.min.css'],
-				tasks: ['cssmin'],
-				options: {
-					debounceDelay: 500
-				}
-			},
-			scripts: {
-				files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
-				tasks: ['jshint', 'concat', 'uglify'],
-				options: {
-					debounceDelay: 500
-				}
-			}
-		},
 		clean: {
 			main: ['release/<%= pkg.version %>']
 		},
@@ -145,9 +62,6 @@ module.exports = function( grunt ) {
 				colors: true,
 				testSuffix: 'Tests.php'
 			}
-		},
-		qunit: {
-			all: ['tests/qunit/**/*.html']
 		}
 	} );
 
@@ -155,17 +69,10 @@ module.exports = function( grunt ) {
 	require('load-grunt-tasks')(grunt);
 
 	// Register tasks
-	
-	grunt.registerTask( 'css', ['cssmin'] );
-	
 
-	grunt.registerTask( 'js', ['jshint', 'concat', 'uglify'] );
+	grunt.registerTask( 'build', ['clean', 'copy', 'compress'] );
 
-	grunt.registerTask( 'default', ['css', 'js' ] );
-
-	grunt.registerTask( 'build', ['default', 'clean', 'copy', 'compress'] );
-
-	grunt.registerTask( 'test', ['phpunit', 'qunit'] );
+	grunt.registerTask( 'test', ['phpunit'] );
 
 	grunt.util.linefeed = '\n';
 };
